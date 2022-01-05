6b8a5c525ec2ca8ef70ae1553973a0b5c8eb0b81
Rick Martin <rmartin1985@att.net>
2022-01-05 10:45:31 -0600

    clicking my name in Nav redirects to homepage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Nav from "../Nav";
 
 function Header() {
     return (
         <header className="flex-row space-between px-1">
             <h1>
-                Richard Martin
+                <Link as={Link} to='/'>Richard Martin</Link>
             </h1>
             <Nav></Nav>
         </header>
