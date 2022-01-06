f31baa6e2cfb0550a2eceea00d535e1e9bcb8cd4
Rick Martin <rmartin1985@att.net>
2022-01-06 10:36:28 -0600

    added hover effect to projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,60 +1,68 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
+import { GitHub } from "@material-ui/icons";
 import { 
-    IconButton,
-    ImageList,
-    ImageListItem,
-    ImageListItemBar,
-    ListSubheader,
-    makeStyles
- } from "@material-ui/core";
+  ImageList,
+  IconButton,
+  ImageListItem,
+  ImageListItemBar,
+ } from "@mui/material";
 
-import { Info } from "@material-ui/icons";
-
+//  import { ThemeProvider, createTheme } from "@mui/system";
 
 
 
 function Project(props) {
-    const projects = props.projects
-    const useStyles = makeStyles(theme => ({
-        clickableIcon: {
-            color: 'green',
-            '&:hover': {
-                color: 'yellow',
-            },
-        },
-    }));
-    
-    const classes = useStyles();
+  const projects = props.projects
+  const useStyles = makeStyles(theme => ({
+    clickableIcon: {
+      color: 'green',
+      '&:hover': {
+        color: 'yellow',
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
+  // const theme = createTheme({
+  //   text: {
+  //     opacity: 0
+  //   }
+  // })
 
   return (
-    <ImageList sx={{ width: 500, height: 450 }}>
-      <ImageListItem key="Subheader" cols={2}>
-        <ListSubheader component="div">December</ListSubheader>
-      </ImageListItem>
-      {projects.map((project) => (
-        <ImageListItem key={project.id}>
-          <img
-            src={`${project.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${project.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={project.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={project.title}
-            subtitle={project.author}
-            actionIcon={
-              <IconButton onClick={event => window.location.href=`${project.link}`} className={classes.clickableIcon}
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${project.title}`}
-                
-              >
-                  <Info  />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+      <ImageList sx={{ width: 'auto', height: 'auto', "& :hover .MuiImageListItem-img": {opacity: 0.3 }, "& :hover .MuiImageListItemBar-root": {opacity: 1 }}}>
+        {projects.map((project) => (
+          <ImageListItem
+             key={project.id}>
+            <img
+              sx={{ opacity: 1 }}
+              src={`${project.img}`}
+              srcSet={`${project.img}`}
+              alt={project.title}
+              loading="lazy"
+            />
+            <ImageListItemBar 
+              sx={{ opacity: 0 }}
+              title={project.title}
+              subtitle={project.author}
+              actionIcon={
+                <IconButton onClick={event => window.location.href = `${project.link}`} className={classes.clickableIcon}
+                  sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                  aria-label={`info about ${project.title}`}
+
+                >
+                  <GitHub />
+                </IconButton>
+              }
+            />
+
+          </ImageListItem>
+
+        ))}
+
+      </ImageList>
   );
 }
 
