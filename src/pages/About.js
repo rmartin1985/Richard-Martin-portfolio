1c44ff76438e97837a367a3058f2af0236c9cb20
Rick Martin <rmartin1985@att.net>
2022-01-10 17:09:11 -0600

    updated the about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,82 +1,53 @@
 import React from "react";
-import { Grid, Box, Typography, Avatar, } from '@mui/material';
+import { Grid, Box, Typography, Avatar, Container } from '@mui/material';
 import { makeStyles } from "@material-ui/core";
 import Typed from 'react-typed';
-import Particles from 'react-tsparticles'
 import rickAvatar from '../assets/hero/new profile.png'
 
-const useStyles = makeStyles(theme => ({
-    title: {
-        color: '#2b2d42'
-    },
-    subtitle: {
-        color: '#ef233c',
-        marginBottom: '3rem',
-    },
+const useStyles = makeStyles({
     typedContainer: {
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
         width: '100vw',
-        textAlign: 'center',
         zIndex: 1
-
     }
-}))
+
+})
 
 function About() {
 
     const classes = useStyles();
     return (
         <>
-            <Box className={classes.typedContainer}>
-                <Grid container justifyContent='center'>
-                    <Avatar
-                        className={classes.avatar}
-                        src={rickAvatar}
-                        alt='Rick Martin'
-                        sx={{ bgcolor: '#2b2d42', width: 200, height: 200 }}
-                    />
-                </Grid>
-                <Typography className={classes.title} variant='h4'>
-                    <Typed strings={['Richard Martin']} typeSpeed={40} showCursor={false} />
-                </Typography>
-                <Typography>
+            <Box className={classes.typedContainer} sx={{ backgroundColor: '#8d99ae' }}>
+                <Container fixed>
+                    <Grid>
+                        <Avatar
+                            src={rickAvatar}
+                            alt='Rick Martin'
+                            sx={{ bgcolor: '#2b2d42', width: 200, height: 200 }}
+                        />
+                        <h2 className="my-5" id="about">about me</h2>
+                        <p>
+                            Hello! I'm Richard. Recently becomming a Full Stack Developer has been an excellent journey of personal and professional growth.
+                            My background has always been in customer service with over a decade in the restaurant industry and most recently in real estate.
+                            Web Development is just another extension of customer service. Highly motivated and dedicated to my clients to listen to their needs,
+                            understand the product they envision, and then be able to create and deliver to them in a timely and efficient manner.
+                            Whether creating a project solo or joining a team and collaborating to create something exciting and functional, I will deligently 
+                            until the final produt is exactly what was asked for.<br></br><br></br>
 
-                </Typography>
-                <Typography className={classes.subtitle} variant='h5'>
-                    <Typed
-                        strings={['Web Design', 'Web Development', 'MERN Stack']}
-                        typeSpeed={40}
-                        backSpeed={60}
-                        loop />
-
-                </Typography>
+                            In my personal life, I like to spend most of my nights hanging in with my beautiful wife, our dog Meatball
+                            the Great Pyrenees, and our cat Jersey the Maine Coon. I love to cook and I am always on the look out for a
+                            new recipe each week. I try to learn a new skill in the kitchen and perfect my craft every time I attempt a
+                            new dish. We are also season ticket holders for the Austin FC soccer team. If I am not cooking for my family
+                            or at a soccer game, then you can bet you will find me at a concert or any one of our Emo nights dancing and
+                            singing to the songs of my childhood.
+                        </p>
+                    </Grid>
+                </Container>
             </Box>
-            <Particles
-                params={{
-                    particles: {
-                        move: {
-                            enable: true
-                        },
-                        links: {
-                            enable: true,
-                            opacity: 0.3
-                        },
-                        size: {
-                            value: 2
-                        },
-                        number: {
-                            value: 45,
-                            density: {
-                                enable: false,
-                                value_area: 900,
-                            }
-                        }
-                    }
-                }}
-            />
         </>
 
 
